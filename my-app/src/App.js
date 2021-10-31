870912644196bee2e3d6a8f01567b59c324d2998
Reuben John <reuben.chacko.john@gmail.com>
2021-10-30 20:31:50 -0700

    first react app with jsx

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App" st>
+      <h1>HelloDojo!</h1>
+      <h2>Things I need to do</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Get fit</li>
+        <li>Be the best father and husband</li>
+        <li>Change the world for the better</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
